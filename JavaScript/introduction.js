428747b210ab9ee209ffa5ba711f1ab780ecdf58
Shivaji singh <shivajisingh54@gmail.com>
2023-05-02 11:53:52 +0530

    Basics of JavaScript

---
+++ JavaScript/introduction.js
@@ -0,0 +1,31 @@
+// 1995s - netscape navigator
+
+// ecma - ecmascript / javasript
+
+// javascript is :
+
+// 1. programming language
+
+// 2. scripting language
+
+// 3. dynamically typed language
+
+// VARIABLES :
+// data_type variable_name = 10;
+
+// data type in js:
+// Primitive datatype :> Numver , String , Boolean , Null , Undefined , Big int , Symbol
+// Non-Primitive datatype :> Object
+
+var a = 10;
+// a = "Shivaji Singh";
+
+if(a === 10){
+    console.log('a is equals to 10')
+}
+else{
+    console.log('a is equals to 10')
+}
+// let r = Symbol('any random messages');
+// var x = null;
+// console.log(a);
