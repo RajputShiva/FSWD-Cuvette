47eec5f33389e51820a23d0b8d1e7e466bd86137
Shivaji singh <shivajisingh54@gmail.com>
2023-05-08 12:16:04 +0530

    DOM in JS

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+// function declaration
+// function nameOfTheFunction (a , b , c){
+//     // body of the function.
+//     console.log();
+// }
+
+// 2. function expression
+
+var fn = function nameOfTheFunction (){
+    // body of the function.
+    console.log(fn);
+}
+
+fn();
+
+// create an array:- array me hum number ,string or boolean value define kr skte hain infect saare datatype kr skte hain.
+
+let arr = [1,2,355,678,"shivaji",true]
